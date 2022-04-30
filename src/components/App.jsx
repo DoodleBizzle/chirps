9978bf378306d28ae82a30dde3c71d056883bfa5
Barry Richard II <barrypaulrichard@gmail.com>
2022-04-30 14:06:32 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// This is just a sample App component, replace it with your own.
+const App = () => {
+
+
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
